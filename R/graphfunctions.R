a94dba34be709acb72afcf9782021b210a5db896
vjcitn <stvjc@channing.harvard.edu>
2021-05-25 14:36:33 -0400

    try to fix issue 1

--- R/graphfunctions.R
+++ R/graphfunctions.R
@@ -147,7 +147,8 @@ setMethod("ugraph", "graph",
 
 setMethod("edgeMatrix", "graphAM",
           function(object, duplicates=FALSE) {
-              to <- apply(object@adjMat, 1, function(x) which(x != 0))
+              to <- apply(object@adjMat, 1, function(x) which(x != 0)) # vector
+              to <- lapply(to, force) # fixup to allow listLen to work
               from <- rep(seq_len(numNodes(object)), listLen(to))
               to <- unlist(to, use.names=FALSE)
               ans <- rbind(from=from, to=to)
